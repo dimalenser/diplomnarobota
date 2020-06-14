73e85d2dd1ad448011e7312c2ff38557db966655
Dmitriy <56391907+dimalenser@users.noreply.github.com>
2020-06-14 11:40:37 +0300

    WebBrowser Bag Fix

--- KolomyyaTrees/KolomyyaTrees/TreeAdd.xaml.cs
+++ KolomyyaTrees/KolomyyaTrees/TreeAdd.xaml.cs
@@ -1,7 +1,9 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.Win32;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Media;
 using System.Text;
@@ -22,14 +24,109 @@ namespace KolomyyaTrees
     /// </summary>
     public partial class TreeAdd : Window
     {
+        private void SetBrowserFeatureControlKey(string feature, string appName, uint value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(
+                String.Concat(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature),
+                RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(appName, (UInt32)value, RegistryValueKind.DWord);
+            }
+        }
+
+        private void SetBrowserFeatureControl()
+        {
+            // http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
+
+            // FeatureControl settings are per-process
+            var fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+
+            // make the control is not running inside Visual Studio Designer
+            if (String.Compare(fileName, "devenv.exe", true) == 0 || String.Compare(fileName, "XDesProc.exe", true) == 0)
+                return;
+
+            SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+            SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
+        }
+
+        private UInt32 GetBrowserEmulationMode()
+        {
+            int browserVersion = 7;
+            using (var ieKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer",
+                RegistryKeyPermissionCheck.ReadSubTree,
+                System.Security.AccessControl.RegistryRights.QueryValues))
+            {
+                var version = ieKey.GetValue("svcVersion");
+                if (null == version)
+                {
+                    version = ieKey.GetValue("Version");
+                    if (null == version)
+                        throw new ApplicationException("Microsoft Internet Explorer is required!");
+                }
+                int.TryParse(version.ToString().Split('.')[0], out browserVersion);
+            }
+
+            UInt32 mode = 11000; // Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE11 Standards mode. Default value for Internet Explorer 11.
+            switch (browserVersion)
+            {
+                case 7:
+                    mode = 7000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE7 Standards mode. Default value for applications hosting the WebBrowser Control.
+                    break;
+                case 8:
+                    mode = 8000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE8 mode. Default value for Internet Explorer 8
+                    break;
+                case 9:
+                    mode = 9000; // Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                    break;
+                case 10:
+                    mode = 10000; // Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE10 mode. Default value for Internet Explorer 10.
+                    break;
+                default:
+                    // use IE11 mode by default
+                    break;
+            }
+
+            return mode;
+        }
         public TreeAdd()
         {
+            SetBrowserFeatureControl();
+
             InitializeComponent();
             
 
             labelTreeCountUpdate();
 
-            
+            if ((bool)GoogleMapsCB.IsChecked == false)
+            {
+                GoogleMapsWB.Visibility = Visibility.Hidden;
+            }
+
+            GoogleMapsWB.Navigate("https://www.google.com.ua/maps/place/%D0%9A%D0%BE%D0%BB%D0%BE%D0%BC%D1%8B%D1%8F,+%D0%98%D0%B2%D0%B0%D0%BD%D0%BE-%D0%A4%D1%80%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+78200/@48.5660049,24.9500774,33477m/");
+
+
             textBoxYears.Text = "Кількість років";
             textBoxYears.Foreground = Brushes.LightGray;
             
@@ -311,5 +408,14 @@ namespace KolomyyaTrees
             labelTreesKPD.Content = $"До нашої бази даних занесено {treeN - 1} дерев";
         }
 
+        private void GoogleMapsCB_Checked(object sender, RoutedEventArgs e)
+        {
+            GoogleMapsWB.Visibility = Visibility.Visible;
+        }
+
+        private void GoogleMapsCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            GoogleMapsWB.Visibility = Visibility.Hidden;
+        }
     }
 }
